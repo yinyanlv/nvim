06a9341d18d5ee13bb86b31a06a9c6a6cbb397b8
bugong <1761869346@qq.com>
2023-05-13 19:17:50 +0800

    update: 修改镜像

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -1,16 +1,3 @@
-local packer = require('packer')
-
-packer.init({
-  package_root = vim.fn.stdpath('data')..'/site/pack',
-  git = {
-    clone_timeout = 600, -- 设置超时时间（可选）
-  },
-  display = {
-    open_fn = require('packer.util').float,
-  },
-  mirror = 'https://mirrors.tuna.tsinghua.edu.cn',
-})
-
 -- 自动安装packer
 local ensure_packer = function()
   local fn = vim.fn
@@ -24,7 +11,7 @@ local ensure_packer = function()
 end
 
 local packer_bootstrap = ensure_packer()
-
+local packer = require('packer')
 
 -- 保存此文件自动更新安装软件
 -- 注意PackerCompile改成了PackerSync
@@ -36,47 +23,63 @@ vim.cmd([[
   augroup end
 ]])
 
-return packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim' -- 主题
-  use {
-    'nvim-lualine/lualine.nvim',  -- 状态栏
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }  -- 状态栏图标
-  }
-  use {
-    'nvim-tree/nvim-tree.lua',  -- 文档树
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- 文档树图标
+return packer.startup({
+  function(use)
+    use 'wbthomason/packer.nvim'
+    use 'folke/tokyonight.nvim' -- 主题
+    use {
+      'nvim-lualine/lualine.nvim',  -- 状态栏
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }  -- 状态栏图标
     }
+    use {
+      'nvim-tree/nvim-tree.lua',  -- 文档树
+      requires = {
+        'nvim-tree/nvim-web-devicons', -- 文档树图标
+      }
+    }
+    use 'christoomey/vim-tmux-navigator' -- 用ctl-hjkl来定位窗口
+    use 'nvim-treesitter/nvim-treesitter' -- 语法高亮
+    use 'p00f/nvim-ts-rainbow' -- 配合treesitter，不同括号颜色区分
+    use {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',  -- 这个相当于mason.nvim和lspconfig的桥梁
+      'neovim/nvim-lspconfig'
+    }
+      -- 自动补全
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'L3MON4D3/LuaSnip' -- snippets引擎，不装这个自动补全会出问题
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'rafamadriz/friendly-snippets'
+    use 'hrsh7th/cmp-path' -- 文件路径
+
+    use 'numToStr/Comment.nvim' -- gcc和gc注释
+    use 'windwp/nvim-autopairs' -- 自动补全括号
+
+    use 'akinsho/bufferline.nvim' -- buffer分割线
+    use 'lewis6991/gitsigns.nvim' -- 左则git提示
+
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    if packer_bootstrap then
+      packer.sync()
+    end
+  end,
+  config = {
+    -- 最大并发数
+    max_jobs = 16,
+    -- 自定义源
+    git = {
+        default_url_format = 'https://mirror.ghproxy.com/https://github.com/%s'
+    }
+    -- display = {
+    -- 使用浮动窗口显示
+    --   open_fn = function()
+    --     return require('packer.util').float({ border = 'single' })
+    --   end,
+    -- },
   }
-  use "christoomey/vim-tmux-navigator" -- 用ctl-hjkl来定位窗口
-  use "nvim-treesitter/nvim-treesitter" -- 语法高亮
-  use "p00f/nvim-ts-rainbow" -- 配合treesitter，不同括号颜色区分
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",  -- 这个相当于mason.nvim和lspconfig的桥梁
-    "neovim/nvim-lspconfig"
-  }
-    -- 自动补全
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "L3MON4D3/LuaSnip" -- snippets引擎，不装这个自动补全会出问题
-  use "saadparwaiz1/cmp_luasnip"
-  use "rafamadriz/friendly-snippets"
-  use "hrsh7th/cmp-path" -- 文件路径
-
-  use "numToStr/Comment.nvim" -- gcc和gc注释
-  use "windwp/nvim-autopairs" -- 自动补全括号
-
-  use "akinsho/bufferline.nvim" -- buffer分割线
-  use "lewis6991/gitsigns.nvim" -- 左则git提示
-
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  if packer_bootstrap then
-    packer.sync()
-  end
-end)
+})
