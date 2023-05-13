817d96feae1dfe95db75da1c2689dc90d05da066
bugong <1761869346@qq.com>
2023-05-14 00:47:42 +0800

    update: 修改cmp

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Neovim config
 ``` text
 1、安装插件代理需要开增强模式
 
-2、遇到git拉取插件因代码冲突等原因导致失败时，可直接进入/Users/bugong/.local/share/nvim/site/pack/packer/start目录下进行插件删除等操作，然后重新install
+2、遇到git拉取插件因代码冲突等原因导致失败时，可直接进入~/.local/share/nvim/site/pack/packer/start目录下进行插件删除等操作，然后重新install
 ```
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,71 +1,60 @@
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then
-  return
-end
-
-local snip_status_ok, luasnip = pcall(require, 'luasnip')
-if not snip_status_ok then
-  return
-end
-
-require('luasnip.loaders.from_vscode').lazy_load()
-
--- 下面会用到这个函数
-local check_backspace = function()
-  local col = vim.fn.col '.' - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
-end
-
+local cmp = require('cmp')
 
 cmp.setup({
+  -- 指定 snippet 引擎
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      -- For `vsnip` users.
+      vim.fn['vsnip#anonymous'](args.body)
+
+      -- For `luasnip` users.
+      -- require('luasnip').lsp_expand(args.body)
+
+      -- For `ultisnips` users.
+      -- vim.fn['UltiSnips#Anon'](args.body)
+
+      -- For `snippy` users.
+      -- require'snippy'.expand_snippet(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-e>'] = cmp.mapping.abort(),  -- 取消补全，esc也可以退出
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      'i',
-      's',
-    }),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      'i',
-      's',
-    }),
-  }),
-
-  -- 这里重要
+  -- 补全源
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    -- For vsnip users.
+    { name = 'vsnip' },
+
+    -- For luasnip users.
+    -- { name = 'luasnip' },
+
+    --For ultisnips users.
+    -- { name = 'ultisnips' },
+
+    -- -- For snippy users.
+    -- { name = 'snippy' },
+  }, { { name = 'buffer' }, { name = 'path' } }),
+
+  -- 快捷键设置
+  mapping = require('core.keymaps').cmp(cmp),
+  -- 使用lspkind-nvim显示类型图标
+  formatting = require('lsp.ui').formatting,
+
+})
+
+-- / 查找模式使用 buffer 源
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+-- : 命令行模式中使用 path 和 cmdline 源.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
     { name = 'path' },
   }, {
-    { name = 'buffer' },
-  })
+    { name = 'cmdline' },
+  }),
 })
+
